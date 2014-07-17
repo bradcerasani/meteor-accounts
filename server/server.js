65850f592990d513bbb94a3088e47814afacd76e
Brad Cerasani <bradcerasani@gmail.com>
2014-07-16 23:46:34 -0600

    added Google oAuth

---
+++ server/server.js
@@ -0,0 +1,5 @@
+// Accounts.loginServiceConfiguration.insert({
+//   service: "google",
+//   clientId: "1036382187706-dr7ahsck5n7e14bce0i4s3ko82dcv5ps.apps.googleusercontent.com",
+//   secret: "uQC2ANOTlg-oYsdbeTTf2Hdm"
+// });
