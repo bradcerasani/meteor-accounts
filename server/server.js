573707c2f56276004712ae03670bbbb60e415bea
Brad Cerasani <bradcerasani@gmail.com>
2014-07-17 22:55:20 -0500

    restricted login to blackpixel.com emails, added onCreateUser event handler

--- server/server.js
+++ server/server.js
@@ -1,5 +1,23 @@
-// Accounts.loginServiceConfiguration.insert({
+ServiceConfiguration.configurations.remove({
+  service: "google"
+});
+
+// ServiceConfiguration.configurations.insert({
 //   service: "google",
-//   clientId: "1036382187706-dr7ahsck5n7e14bce0i4s3ko82dcv5ps.apps.googleusercontent.com",
-//   secret: "uQC2ANOTlg-oYsdbeTTf2Hdm"
+//   clientId: "***",
+//   secret: "***"
 // });
+
+Accounts.config({
+  restrictCreationByEmailDomain: 'blackpixel.com'
+});
+
+Accounts.onCreateUser(function(options, user) {
+  check(options, Object);
+  check(user, Object);
+
+  console.log(options);
+  console.log(user);
+
+  return user;
+});
